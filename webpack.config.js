8cf4e23592eb2a36d7a7c5ce78138ec9d1510598
triestpa <patrick.triest@gmail.com>
2017-08-05 15:40:17 +0700

    Adjust webpack config to allow standard browser script imports

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const BabiliPlugin = require("babili-webpack-plugin");
 module.exports = {
     entry: "./otp.js",
     output: {
-        filename: "otp.min.js"
+        filename: "otp.min.js",
+        library: 'OTP',
     },
     module: {
         loaders: [
@@ -13,11 +14,11 @@ module.exports = {
                 loader: "babel-loader",
                 include: [ path.resolve(__dirname, "src"), ],
                 test: /\.js$/,
-                query: { presets: ['es2016'] }
+                query: { presets: ['es2017'] }
             },
         ]
     },
     plugins: [
-        new BabiliPlugin({})
+      //  new BabiliPlugin({})
     ]
 };
